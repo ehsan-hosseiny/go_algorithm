09f0e5da5b5a9e1e675aa9ce31324a194dcab51a
pc <ehsanhossini@gmail.com>
2024-12-29 16:52:15 +0330

    merge two list

--- main.go
+++ main.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
-
-	ValidParantheses "github.com/ehsan-hosseiny/algorythm/valid_parantheses"
+	mergetwolists "github.com/ehsan-hosseiny/algorythm/mergetwolists"
 )
 
-func main() {
-	d := ValidParantheses.IsValid("[]")
-	fmt.Println(d)
+func createLinkedList(nums []int) *mergetwolists.ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	head := &mergetwolists.ListNode{Val: nums[0]}
+	current := head
+
+	for _, val := range nums[1:] {
+		current.Next = &mergetwolists.ListNode{Val: val}
+		current = current.Next
+	}
+
+	return head
+}
+
+func printLinkedList(head *mergetwolists.ListNode) {
+	current := head
+	for current != nil {
+		fmt.Print(current.Val, " ")
+		current = current.Next
+	}
+	fmt.Println()
+}
+
+func main() { // Create two linked lists
+	list1 := createLinkedList([]int{1, 2, 4})
+	list2 := createLinkedList([]int{1, 3, 4})
+
+	// Merge the two linked lists
+	mergedList := mergetwolists.mergeTwoLists(list1, list2)
+
+	// Print the merged linked list
+	printLinkedList(mergedList) // Output: 1 1 2 3 4 4
 
 }
