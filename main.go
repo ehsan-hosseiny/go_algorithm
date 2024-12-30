9ccfa78939a873a35c23326ad68e5dab74c89125
pc <ehsanhossini@gmail.com>
2024-12-30 11:56:09 +0330

    find index first occurrence

--- main.go
+++ main.go
@@ -2,42 +2,16 @@ package main
 
 import (
 	"fmt"
-	mergetwolists "github.com/ehsan-hosseiny/algorythm/mergetwolists"
+
+	findfirstoccurrence "github.com/ehsan-hosseiny/algorythm/find_index_first_occurrence"
 )
 
-func createLinkedList(nums []int) *mergetwolists.ListNode {
-	if len(nums) == 0 {
-		return nil
-	}
-
-	head := &mergetwolists.ListNode{Val: nums[0]}
-	current := head
-
-	for _, val := range nums[1:] {
-		current.Next = &mergetwolists.ListNode{Val: val}
-		current = current.Next
-	}
-
-	return head
-}
-
-func printLinkedList(head *mergetwolists.ListNode) {
-	current := head
-	for current != nil {
-		fmt.Print(current.Val, " ")
-		current = current.Next
-	}
-	fmt.Println()
-}
-
 func main() { // Create two linked lists
-	list1 := createLinkedList([]int{1, 2, 4})
-	list2 := createLinkedList([]int{1, 3, 4})
 
-	// Merge the two linked lists
-	mergedList := mergetwolists.mergeTwoLists(list1, list2)
+	haystack2 := "leetcodeleet"
+	needle2 := "le"
 
-	// Print the merged linked list
-	printLinkedList(mergedList) // Output: 1 1 2 3 4 4
+	indices2 := findfirstoccurrence.StrStr(haystack2, needle2)
+	fmt.Println(indices2) // Output: []
 
 }
